c33668e4671628790b3c740f03b7f947013484e4
Ted Michael Celis <krazete@gmail.com>
2019-01-26 15:35:33 -0800

    started building gui, broke previouw working model

--- index.js
+++ index.js
@@ -90,30 +90,94 @@ function noCompose(src, x, y, scale) {
 }
 
 function init() {
-    canvas = document.getElementById("canvas");
+    canvas = document.getElementById("canvas-0");
     context = canvas.getContext("2d");
     canvas.width = 500;
     canvas.height = 800;
     Promise.all([
         getImageData("fragment/GreyBackground.png"),
-        getImageData("gradient/30-3889.png", true)
+
+        getImageData("fragment/BronzeTop.png"),
+        getImageData("fragment/BronzeElement.png"),
+        getImageData("fragment/BronzeLevel.png"),
+        getImageData("fragment/BronzeBottom.png"),
+
+        getImageData("fragment/SilverTop.png"),
+        getImageData("fragment/SilverElement.png"),
+        getImageData("fragment/SilverLevel.png"),
+        getImageData("fragment/SilverBottom.png"),
+
+        getImageData("fragment/GoldTop.png"),
+        getImageData("fragment/GoldElement.png"),
+        getImageData("fragment/GoldLevel.png"),
+        getImageData("fragment/GoldBottom.png"),
+
+        getImageData("fragment/DiamondTop.png"),
+        getImageData("fragment/DiamondElement.png"),
+        getImageData("fragment/DiamondLevel.png"),
+        getImageData("fragment/DiamondBottom.png"),
+
+        getImageData("gradient/DiamondGradientMapFireBackplate.png", true),
+        getImageData("gradient/DiamondGradientWaterBackplate.png", true),
+        getImageData("gradient/DiamondGradientMapWindBackplate.png", true),
+        getImageData("gradient/DiamondGradientLightBackplate.png", true),
+        getImageData("gradient/DiamondGradientDarkBackplate.png", true),
+        getImageData("gradient/DarkGradient.png", true),
+
+        getImageData("gradient/DiamondGradientMapFire.png", true),
+        getImageData("gradient/DiamondGradientWater.png", true),
+        getImageData("gradient/DiamondGradientMapWind.png", true),
+        getImageData("gradient/DiamondGradientLight.png", true),
+        getImageData("gradient/DiamondGradientDark.png", true),
+        getImageData("gradient/DiamondGradientMapNeutralB.png", true)
     ]).then(function (responses) {
-        compose(responses[0], responses[1], 25, 48, 1.16);
-        noCompose("art/zozo.png", -210, -70, 1).then(function () {
-            Promise.all([
-                getImageData("fragment/DiamondTop.png"),
-                getImageData("fragment/DiamondElement.png"),
-                getImageData("fragment/DiamondLevel.png"),
-                getImageData("fragment/DiamondBottom.png"),
-                getImageData("gradient/DiamondGradientLight.png", true),
-            ]).then(function (responses) {
-                compose(responses[0], responses[4], 0, 0);
-                compose(responses[1], responses[4], 0, -2.5);
-                compose(responses[2], responses[4], 273, 0, 0.56);
-                compose(responses[3], responses[4], 0, 282);
-                noCompose("fragment/ElementalIconLight.png", 13, 11, 0.43);
-            })
-        });
+        backgroundData = responses[0];
+
+        bronzeTopData = responses[1];
+        bronzeElementData = responses[2];
+        bronzeLevelData = responses[3];
+        bronzeBottomData = responses[4];
+
+        silverTopData = responses[5];
+        silverElementData = responses[6];
+        silverLevelData = responses[7];
+        silverBottomData = responses[8];
+
+        goldTopData = responses[9];
+        goldElementData = responses[10];
+        goldLevelData = responses[11];
+        goldBottomData = responses[12];
+
+        diamondTopData = responses[13];
+        diamondElementData = responses[14];
+        diamondLevelData = responses[15];
+        diamondBottomData = responses[16];
+
+        fireData = responses[17];
+        waterData = responses[18];
+        windData = responses[19];
+        lightData = responses[20];
+        darkData = responses[21];
+        neutralData = responses[22];
+
+        diamondFireData = responses[23];
+        diamondWaterData = responses[24];
+        diamondWindData = responses[25];
+        diamondLightData = responses[26];
+        diamondDarkData = responses[27];
+        diamondNeutralData = responses[28];
+
+        compose(backgroundData, fireData, 25, 48, 1.16);
+        // noCompose("art/zozo.png", -210, -70, 1);
+        compose(diamondTopData, diamondFireData, 0, 0);
+        compose(diamondElementData, diamondFireData, 0, -2.5);
+        compose(diamondLevelData, diamondFireData, 273, 0, 0.56);
+        compose(diamondBottomData, diamondFireData, 0, 282);
+        noCompose("fragment/ElementalIconLight.png", 13, 11, 0.43);
+
+        for (var i = 0; i < 10; i++) {
+            noCompose("fragment/EnergyIcon-Blue.png", 55 + 24 * i, 466, 0.85);
+        }
     });
 }
 
