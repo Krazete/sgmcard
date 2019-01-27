db25bf3f6dcaaaf993e67d1b8852266833196c3d
Ted Michael Celis <krazete@gmail.com>
2019-01-26 20:59:34 -0800

    generalized colorization process
    and also broke all previous colorization code

--- index.js
+++ index.js
@@ -89,11 +89,94 @@ function noCompose(src, x, y, scale) {
     return new Promise(request);
 }
 
+function loadImage(src) {
+    function request(resolve, reject) {
+        var image = new Image();
+        image.addEventListener("load", function () {
+            resolve(this);
+        });
+        image.addEventListener("error", function () {
+            reject(this);
+        });
+        image.src = src;
+    }
+    return new Promise(request);
+}
+
+function getImageDataFromImage(image, w, h) {
+    var tempCanvas = document.createElement("canvas");
+    var tempContext = tempCanvas.getContext("2d");
+    if (w && h) {
+        tempCanvas.width = w;
+        tempCanvas.height = h;
+        tempContext.drawImage(image, 0, 0, w, h);
+    }
+    else {
+        tempCanvas.width = image.width;
+        tempCanvas.height = image.height;
+        tempContext.drawImage(image, 0, 0);
+    }
+    return tempContext.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+}
+
+function colorizeImageDataWithGradientData(imageData, gradientData) {
+    var colorizedData = new ImageData(imageData.width, imageData.height);
+    var pixels = imageData.data.length / 4;
+    for (var i = 0; i < pixels; i++) {
+        var r = imageData.data[4 * i];
+        var g = imageData.data[4 * i + 1];
+        var b = imageData.data[4 * i + 2];
+        var intensity = Math.floor((r + g + b) / 3);
+        var a = imageData.data[4 * i + 3];
+        imageData.data[4 * i] = gradientData.data[4 * intensity];
+        imageData.data[4 * i + 1] = gradientData.data[4 * intensity + 1];
+        imageData.data[4 * i + 2] = gradientData.data[4 * intensity + 2];
+        imageData.data[4 * i + 3] = a;
+    }
+    return imageData;
+}
+
+function getImageFromImageData(imageData) {
+    var tempCanvas = document.createElement("canvas");
+    var tempContext = tempCanvas.getContext("2d");
+    tempCanvas.width = imageData.width;
+    tempCanvas.height = imageData.height;
+    tempContext.putImageData(imageData, 0, 0);
+    return loadImage(tempCanvas.toDataURL());
+}
+
+function loadColorizedImage(imageSrc, gradientSrc) {
+    return Promise.all([
+        loadImage(imageSrc),
+        loadImage(gradientSrc)
+    ]).then(function (response) {
+        var imageData = getImageDataFromImage(response[0]);
+        var gradientData = getImageDataFromImage(response[1], 256, 1);
+        var colorizedData = colorizeImageDataWithGradientData(imageData, gradientData);
+        return getImageFromImageData(colorizedData);
+    });
+}
+
+function loadBackground() {
+    if (checked) {
+        loadImage("fragment/GoldTop.png").then(console.log);
+    }
+    else {
+        loadColorizedImage("fragment/GoldTop.png", "gradient/DiamondGradientDark.png").then(console.log);
+    }
+}
+
+function drawImageToCanvas(image, canvas, x, y, w, h) {
+    var context = canvas.getContext("2d");
+    if (w && h) {
+        context.drawImage(image, x, y);
+    }
+    else {
+        context.drawImage(image, x, y, w, h);
+    }
+}
+
 function init() {
-    canvas = document.getElementById("canvas-0");
-    context = canvas.getContext("2d");
-    canvas.width = 500;
-    canvas.height = 800;
     Promise.all([
         getImageData("fragment/GreyBackground.png"),
 
@@ -130,55 +213,7 @@ function init() {
         getImageData("gradient/DiamondGradientLight.png", true),
         getImageData("gradient/DiamondGradientDark.png", true),
         getImageData("gradient/DiamondGradientMapNeutralB.png", true)
-    ]).then(function (responses) {
-        backgroundData = responses[0];
-
-        bronzeTopData = responses[1];
-        bronzeElementData = responses[2];
-        bronzeLevelData = responses[3];
-        bronzeBottomData = responses[4];
-
-        silverTopData = responses[5];
-        silverElementData = responses[6];
-        silverLevelData = responses[7];
-        silverBottomData = responses[8];
-
-        goldTopData = responses[9];
-        goldElementData = responses[10];
-        goldLevelData = responses[11];
-        goldBottomData = responses[12];
-
-        diamondTopData = responses[13];
-        diamondElementData = responses[14];
-        diamondLevelData = responses[15];
-        diamondBottomData = responses[16];
-
-        fireData = responses[17];
-        waterData = responses[18];
-        windData = responses[19];
-        lightData = responses[20];
-        darkData = responses[21];
-        neutralData = responses[22];
-
-        diamondFireData = responses[23];
-        diamondWaterData = responses[24];
-        diamondWindData = responses[25];
-        diamondLightData = responses[26];
-        diamondDarkData = responses[27];
-        diamondNeutralData = responses[28];
-
-        compose(backgroundData, fireData, 25, 48, 1.16);
-        // noCompose("art/zozo.png", -210, -70, 1);
-        compose(diamondTopData, diamondFireData, 0, 0);
-        compose(diamondElementData, diamondFireData, 0, -2.5);
-        compose(diamondLevelData, diamondFireData, 273, 0, 0.56);
-        compose(diamondBottomData, diamondFireData, 0, 282);
-        noCompose("fragment/ElementalIconLight.png", 13, 11, 0.43);
-
-        for (var i = 0; i < 10; i++) {
-            noCompose("fragment/EnergyIcon-Blue.png", 55 + 24 * i, 466, 0.85);
-        }
-    });
+    ]);
 }
 
 window.addEventListener("DOMContentLoaded", init);
